9de313f51cf81e02bcecfa6eab83c82151623b14
lightzane <bhest.pat@gmail.com>
2021-06-12 18:38:50 +0800

    added alias

--- cypress/integration/tests/Google/google.js
+++ cypress/integration/tests/Google/google.js
@@ -7,9 +7,15 @@ Given('I open Google page', () => {
 });
 
 When(`I type {string} in the textfield`, (keyword) => {
-    cy.get('.gLFyf').type(keyword).type('{enter}');
-});
-
-When('I click on image tab', () => {
-    cy.contains('Images').click();
+    cy.get('.gLFyf').as('searchField'); // the DOM element has now an alias of 'searchField'
+    cy.get('@searchField')
+        .type(keyword)
+        .then(($nav) => {
+            expect($nav.val()).eq('IU pretty');
+            cy.log('Use .val() to get values from Input elements');
+            cy.log('Use .text() to get value from non-Input elements?');
+        });
+    cy.get('@searchField').type('{enter}');
+    cy.get('.GmE3X').as('labelImageResult'); // the DOM element has now an alias of 'labelImageResult'
+    cy.get('@labelImageResult').should('have.text', `Images for ${keyword}`);
 });
